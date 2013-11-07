3006b2c5a8f8ffb4ca133cea4f1fb4b6c666c345
Harold Barker <git@haroldbarker.co.uk>
2013-11-07 10:19:03 +0000

    Allow syncing with remote server.

--- sync.sh
+++ sync.sh
@@ -2,15 +2,19 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".gitignore" --exclude "init/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
+    if [[ -n $1 ]]; then
+        rsync --exclude ".git/" --exclude ".gitignore" --exclude "init/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . $1:~
+    else
+        rsync --exclude ".git/" --exclude ".gitignore" --exclude "init/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
+    fi
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	doIt $2
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
+		doIt $1
 	fi
 fi
 unset doIt
