d83aba58b28c3ad3ceb542a5521ffc85c10fcb1c
Harold Barker <harold.barker@jadu.net>
2015-03-03 16:43:35 +0000

    Disable ctrl-⌘-D keyboard shortcut

--- init/setup.sh
+++ init/setup.sh
@@ -4,4 +4,7 @@ set -e
 # Enable selection in quick look
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
+# Disable ctrl-⌘-D keyboard shortcut
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 70 '<dict><key>enabled</key><false/></dict>'
+
 killall Finder
