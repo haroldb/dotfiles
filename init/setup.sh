28aef070ddb0b0856e47145fcc6007125829273a
Harold Barker <harold.barker@jadu.net>
2015-10-02 13:16:44 +0100

    Disable bouncing dock icons

--- init/setup.sh
+++ init/setup.sh
@@ -7,4 +7,7 @@ defaults write com.apple.finder QLEnableTextSelection -bool true
 # Disable ctrl-⌘-D keyboard shortcut
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 70 '<dict><key>enabled</key><false/></dict>'
 
+# Disable bouncing dock icons
+defaults write com.apple.dock no-bouncing -bool true
+
 killall Finder
